d010f68ff4a46f34d97808a318741fb0afe50466
TrystanLea <trystan.lea@gmail.com>
2024-01-22 15:25:28 +0000

    updated ap start implementation

--- scripts/startAP.sh
+++ scripts/startAP.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 
+    echo "Try \"sudo $0\""	
+    exit 1
+fi
+
 # Check if the interface ap0 exists
 if ip link show ap0 > /dev/null 2>&1; then
     echo "The interface 'ap0' already exists."
 else
     echo "The interface 'ap0' does not exist. Creating the interface..."
-    sudo iw dev wlan0 interface add ap0 type __ap
+    iw dev wlan0 interface add ap0 type __ap
     # Add any additional commands to configure the interface here
 fi
 
 sleep 1
 
-# sudo nmcli dev wifi hotspot ifname ap0 ssid emonPi password emonpi2016
+# dnsmasq captive portal settings
+filename='/etc/NetworkManager/dnsmasq.d/redirect.conf'
+cat > $filename <<-EOF
+address=/#/192.168.42.1
+EOF
 
-sudo nmcli con delete hotspot
+# sudo nmcli dev wifi hotspot ifname ap0 ssid emonPi password emonpi2016
+nmcli con delete hotspot
 
 # Step 1: Create a new Wi-Fi connection profile
-sudo nmcli con add type wifi ifname ap0 con-name hotspot autoconnect no ssid emonPi
+nmcli con add type wifi ifname ap0 con-name hotspot autoconnect no ssid emonSD
 
 # Set Wi-Fi security (Assuming WPA-PSK here)
-sudo nmcli con modify hotspot 802-11-wireless-security.key-mgmt wpa-psk
-sudo nmcli con modify hotspot 802-11-wireless-security.psk emonpi2016
+nmcli con modify hotspot 802-11-wireless-security.key-mgmt wpa-psk
+nmcli con modify hotspot 802-11-wireless-security.psk emonpi2016
 
 # Step 2: Modify the connection with static IP settings
-sudo nmcli con modify hotspot ipv4.addresses 192.168.42.1/24
-sudo nmcli con modify hotspot ipv4.gateway 192.168.42.1
-sudo nmcli con modify hotspot ipv4.dns 8.8.8.8
-sudo nmcli con modify hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method manual
+nmcli con modify hotspot ipv4.addresses 192.168.42.1/24
+
+# sudo nmcli con modify hotspot ipv4.gateway 192.168.42.1
+# sudo nmcli con modify hotspot ipv4.dns 8.8.8.8
+nmcli con modify hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 
 # Step 3: Start the connection
-sudo nmcli con up hotspot
+nmcli con up hotspot
