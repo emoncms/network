86b365a44ec8f2f40bbf267d4ec0777a289f5a9c
TrystanLea <trystan.lea@gmail.com>
2024-01-29 17:56:41 +0000

    change back to sudo requirement

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+
+### Check if run as root ############################
+# This blocks install at modules stage
+# so that this script is ran later
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root" 
+	echo "Try \"sudo $0\""	
+	exit 1
+fi
+
 # ------------------------------------------------------
 # Install network-sudoers
 # ------------------------------------------------------
 filename=/etc/sudoers.d/network-sudoers
-sudo cat > $filename <<-EOF                                                                                   
+cat > $filename <<-EOF                                                                                   
 www-data ALL=(ALL) NOPASSWD:/opt/emoncms/modules/network/scripts/wifi_connect.sh
 www-data ALL=(ALL) NOPASSWD:/opt/emoncms/modules/network/scripts/wifi_rescan.sh
 www-data ALL=(ALL) NOPASSWD:/opt/emoncms/modules/network/scripts/startAP.sh
